dde115f23d018e49dff7a17a23ba281559117144
amir0o0apm <amirsarani996@gmail.com>
2024-07-03 12:53:52 +0330

    تدوین نهایی

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,4 +1,7 @@
 
+// روش محاسباتی نوع دوم
+
+
 function click3(x) {
   var display1=document.getElementById("result").innerHTML;
   if (display1 == "0") { display1=''}
@@ -6,6 +9,7 @@ function click3(x) {
   document.getElementById("result").innerHTML=display1;
 
 }
+
 function newsolve(){
   var result =document.getElementById("result").innerHTML
   document.getElementById("result").innerHTML=eval(result);
@@ -32,32 +36,23 @@ function delchar(){
  // ==================================================================
   
 
+// روش محاسباتی نوع اول
+
 function click1(x) {
 
   var display1 =document.getElementById("display1");
   var result2 = document.getElementById("result2");
   var display2 = document.getElementById("display2");
   display1.innerHTML = result2.innerHTML;
+  result2.innerHTML= 0;
   display2.innerHTML = x; 
 
 }
   
   function click2(x){
-  
-  //  var selectresult = document.getElementById('result');   
-  //  selectresult.innerHTML=selectresult.innerHTML+x;
-
   var a =parseInt(document.getElementById('result2').innerHTML);
   var c = a*10+x
   document.getElementById('result2').innerHTML = c;
-
-
-  //  var selectresult = document.getElementById('result');   
-  //  selectresult.innerHTML=selectresult.innerHTML-x;
-  
-  //  var calculate =+x
-  
-  //  document.getElementById('result').innerHTML=calculate;
   }
   
   
@@ -66,7 +61,7 @@ function click1(x) {
   var display1 = parseInt(document.getElementById("display1").innerHTML);
   var result2 = parseInt(document.getElementById("result2").innerHTML);
   var display2 = document.getElementById("display2").innerHTML;
-  var c = 0;
+  var c = 0 ;
 
 
   switch (display2) {
