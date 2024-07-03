71178b3bf15cb5614d22831d275c623452970afb
amir0o0apm <amirsarani996@gmail.com>
2024-07-03 09:35:25 +0330

    تکمیل ماشین حساب

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,94 +1,105 @@
 
+function click3(x) {
+  var display1=document.getElementById("result").innerHTML;
+  if (display1 == "0") { display1=''}
+  display1=display1+x;
+  document.getElementById("result").innerHTML=display1;
+
+}
+function newsolve(){
+  var result =document.getElementById("result").innerHTML
+  document.getElementById("result").innerHTML=eval(result);
+}
+
+
+function pak2(){
+  var result = document.getElementById("result");
+  result.innerHTML= 0;
+
+}
+
+function delchar(){
+  var result =document.getElementById("result").innerHTML;
+
+ var r = result.substring(0,result.length-1);
+ document.getElementById("result").innerHTML=r;
+
+}
+
+
+
+
+ // ==================================================================
+  
+
 function click1(x) {
 
   var display1 =document.getElementById("display1");
-  var result = document.getElementById("result");
+  var result2 = document.getElementById("result2");
   var display2 = document.getElementById("display2");
-  display1.innerHTML = result.innerHTML;
+  display1.innerHTML = result2.innerHTML;
   display2.innerHTML = x; 
 
-  result.innerHTML = result.innerHTML;
-  result.innerHTML = x;
-
-  }
+}
   
   function click2(x){
   
-   var selectresult = document.getElementById('result');   
-   selectresult.innerHTML=selectresult.innerHTML+x;
+  //  var selectresult = document.getElementById('result');   
+  //  selectresult.innerHTML=selectresult.innerHTML+x;
 
-  var a =parseInt(document.getElementById('result').innerHTML);
+  var a =parseInt(document.getElementById('result2').innerHTML);
   var c = a*10+x
-  document.getElementById('result').innerHTML = c;
+  document.getElementById('result2').innerHTML = c;
 
 
-   var selectresult = document.getElementById('result');   
-   selectresult.innerHTML=selectresult.innerHTML-x;
+  //  var selectresult = document.getElementById('result');   
+  //  selectresult.innerHTML=selectresult.innerHTML-x;
   
-   var calculate =+x
+  //  var calculate =+x
   
-   document.getElementById('result').innerHTML=calculate;
+  //  document.getElementById('result').innerHTML=calculate;
   }
   
   
-  function solve(x){
+  function solve(){
   
   var display1 = parseInt(document.getElementById("display1").innerHTML);
-  var result = parseInt(document.getElementById("result").innerHTML);
+  var result2 = parseInt(document.getElementById("result2").innerHTML);
   var display2 = document.getElementById("display2").innerHTML;
-  var c =0
+  var c = 0;
 
 
-  switch (result) {
+  switch (display2) {
     case '+':
-      c= result+result
+      c= display1+result2
       break;
       case '-':
-        c= result-result
+        c= display1-result2
         break;
         case '*':
-        c= result*result
+        c= display1*result2
         break;
         case '/':
-        c= result/result
+        c= display1/result2
         break;
     default:
      c = "مقدار وارد شود"
       break;
   }
   
-  document.getElementById("result").innerHTML=c
+  document.getElementById("result2").innerHTML=c
 
   }
   
-  function pak(x){
+  function pak(){
 
     var display1 =document.getElementById("display1");
-    var result = document.getElementById("result");
+    var result2 = document.getElementById("result2");
     var display2 = document.getElementById("display2");
 
-display1.innerHTML=0;
-display2.innerHTML='';
-result.innerHTML= 0;
-  }
-  
-  function click3(x) {
-    var display1=document.getElementById("result").innerHTML;
-    if (display1 == "0") { display1=''}
-    display1=display1+x;
-    document.getElementById("result").innerHTML=display1;
+    display1.innerHTML=0;
+    display2.innerHTML='';
+    result2.innerHTML=0;
 
   }
-  function newsolve(){
-    var result =document.getElementById("result").innerHTML
-    document.getElementById("result").innerHTML=eval(result);
-  }
 
-
-  function delchar(){
-    var result =document.getElementById("result").innerHTML;
-
-   var r = result.substring(0,result.length-1);
-   document.getElementById("result").innerHTML=r;
-
-  }
